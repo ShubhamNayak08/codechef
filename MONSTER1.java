6927ee658508824947186ea452f6f2b4304445b4
ShubhamNayak08 <102741748+ShubhamNayak08@users.noreply.github.com>
2022-12-26 17:01:51 +0530

    Add files via upload

---
+++ MONSTER1.java
@@ -0,0 +1,34 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    Scanner sc = new Scanner(System.in);
+	    int n = sc.nextInt();
+	    
+	    while(n-->0){
+	        
+	        
+	         long H = sc.nextLong(); 
+	        
+	         long X = sc.nextLong(); 
+	        
+	         long Y = sc.nextLong(); 
+	         
+	         if(X>Y && H>0){
+	             System.out.println("1");
+	         }
+	         else{
+	             System.out.println("0");
+	         }
+	   
+	    }
+	    
+	}
+}
